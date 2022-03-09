d24c7f68cde21b7f91c2634bd742c80f2e11b0c5
Tomaz <tomazkunst@gmail.com>
2022-03-09 09:42:39 +0100

    start parsing from last session

--- parlaparser/parse_sessions.py
+++ parlaparser/parse_sessions.py
@@ -89,7 +89,7 @@ class SessionParser(object):
 
             # load type of subjects
             num_of_session = len(data[url_group['root_key']]['SEJA'])
-            for index, session in enumerate(data[url_group['root_key']]['SEJA']):
+            for index, session in enumerate(list(reversed(data[url_group['root_key']]['SEJA']))):
                 print()
                 print('New session')
                 print(session['KARTICA_SEJE']['KARTICA_OZNAKA'])
