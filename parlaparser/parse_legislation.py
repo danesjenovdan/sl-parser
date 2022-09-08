62f1df51bdd3879e8baa69e238fc369718f0b495
Tomaž kunst <cofek0@gmail.com>
2022-09-08 18:39:29 +0200

    added mandate to legislation

--- parlaparser/parse_legislation.py
+++ parlaparser/parse_legislation.py
@@ -177,6 +177,7 @@ class LegislationParser(object):
                         'uid': unid,
                         'timestamp': date_iso,
                         'classification': self.legislation_storage.get_legislation_classifications_id(legislation_file['type'])
+                        'mandate_id': self.storage.mandate_id
                     },
                     document_unids
                 )
