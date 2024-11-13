7150e8eed936c7f81ef79a32bd3a831dde52f450
Tomaž <cofek0@gmail.com>
2024-11-13 11:26:28 +0100

    order imports

--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 import os
-
 import sentry_sdk
-from sentry_sdk import capture_exception
+
 from parladata_base_api.storages.legislation_storage import LegislationConsideration
 from parladata_base_api.storages.session_storage import Session
 from parladata_base_api.storages.storage import DataStorage
@@ -47,21 +46,21 @@ try:
     parse_sifrant = MembershipsParser(storage)
     parse_sifrant.parse()
 except Exception as e:
-    capture_exception(e)
+    sentry_sdk.capture_exception(e)
 
 # session votes / speeches
 try:
     session_parser = SessionParser(storage)
     session_parser.parse(parse_speeches=True, parse_votes=False)
 except Exception as e:
-    capture_exception(e)
+    sentry_sdk.capture_exception(e)
 
 
 try:
     session_parser = VotesParser(storage)
     session_parser.parse()
 except Exception as e:
-    capture_exception(e)
+    sentry_sdk.capture_exception(e)
 
 # use this for parse specific session
 # session_parser.parse(session_number='69', session_type='Izredna', parse_speeches=True, parse_votes=True)
@@ -71,7 +70,7 @@ try:
     question_parser = QuestionParser(storage)
     question_parser.parse()
 except Exception as e:
-    capture_exception(e)
+    sentry_sdk.capture_exception(e)
 
 # Reload data storage for new session key for legislation
 Session.keys = ["name", "organizations"]
