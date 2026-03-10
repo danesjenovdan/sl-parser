fa2a1790592b69a70235900220820df7a1a33dff
Tomaž Kunst <tomazkunst@gmail.com>
2026-03-10 14:52:03 +0100

    add data validation call

--- run_nightly_parser_flow.sh
+++ run_nightly_parser_flow.sh
@@ -24,4 +24,6 @@ echo "update votes to solr"
 python manage.py upload_votes_to_solr
 echo "send notifications"
 python manage.py send_daily_notifications
+echo "run tests"
+python manage.py run_data_validation
 
