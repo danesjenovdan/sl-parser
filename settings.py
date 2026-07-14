b86d246143b80444382e88d60ad83b687277a91e
Tomaž <cofek0@gmail.com>
2026-07-14 08:14:54 +0200

    Convert MAIN_ORG_ID to integer from environment variable

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ API_URL = os.getenv("PARSER_PARLADATA_API_URL", "http://localhost:8000/v3")
 MANDATE_STARTIME = datetime.strptime(
     os.getenv("PARSER_MANDATE_START_DATE", "2026-04-10"), "%Y-%m-%d"
 )
-MAIN_ORG_ID = os.getenv("PARSER_MAIN_ORG_ID", 590)
+MAIN_ORG_ID = int(os.getenv("PARSER_MAIN_ORG_ID", 590))
 MANDATE = os.getenv("PARSER_MANDATE_ID", "4")
 MANDATE_GOV_ID = os.getenv("PARSER_MANDATE_GOV_ID", "X")
 BASE_URL = "https://www.dz-rs.si"
